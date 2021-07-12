24ea9567c5cb97a41b22c6ef1d5b04db127ada48
OwerHub <owerwild@gmail.com>
2021-07-12 07:53:40 +0200

    reads works!

--- frontend/src/components/OneDay.jsx
+++ frontend/src/components/OneDay.jsx
@@ -9,19 +9,24 @@ function OneDay(props) {
   let month = parseInt(date.substring(0, 2));
   let day = parseInt(date.substring(2, 4));
 
-  props.dayTypes.length ? console.log(props.dayTypes) : console.log("not yet");
+  /* props.dayTypes.length ? console.log(props.dayTypes) : console.log("not yet");
+  console.log(dayTypesNew); */
 
   return (
     <div
       className="onedayDiv"
       style={{
         //backgroundColor: dayTypesNew[props.data.dayType].color,
-        backgroundColor: dayTypesNew[props.data.type].color,
+        backgroundColor:
+          props.dayTypes.length && props.dayTypes[props.data.type].color,
         display: !!parseInt(props.selectByte[props.data.type]) ? "block" : "none",
       }}
     >
       <div className="nameDiv">
-        <div className="nameUp">{dayTypesNew[props.data.type].name}</div>
+        {/* <div className="nameUp">{dayTypesNew[props.data.type].name}</div> */}
+        <div className="nameUp">
+          {props.dayTypes.length && props.dayTypes[props.data.type].name}
+        </div>
         <div className="nameBig">{props.data.name}</div>
       </div>
 
