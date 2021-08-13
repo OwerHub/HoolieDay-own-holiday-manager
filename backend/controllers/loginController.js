3720ddf6cd5f6c8c1d88b7d57028836d6caee7fc
OwerHub <owerwild@gmail.com>
2021-08-13 03:07:19 +0200

    Bad request task

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -1,6 +1,31 @@
-const googleFetch = (code) => {
-  console.log(code);
-  console.log("env", process.env.CLIENT_SECRET);
+const fetch = require("node-fetch");
+
+const googleFetch = async (code) => {
+  // console.log(code);
+  //console.log("env", process.env.CLIENT_SECRET);
+
+  const fetchHeaders = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+
+  const fetchBody = {
+    code: code,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    redirect_uri: "http://localhost:3000",
+    grant_type: "authorization_code",
+  };
+
+  const googleFetchUrl = "https://oauth2.googleapis.com/token";
+
+  const response = await fetch(googleFetchUrl, {
+    headers: fetchHeaders,
+    method: "POST",
+    body: JSON.stringify(fetchBody),
+  });
+
+  console.log(response);
 };
 
 exports.testFunct = (req, res) => {
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function Login() {
-  const urlScopeCalendar = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=856443790783-ur90d5sq8jvuouqjabb91npiide0k9p9.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/calendar&redirect_uri=http%3A//localhost:3000/login`;
+  const urlScopeCalendar = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=856443790783-ur90d5sq8jvuouqjabb91npiide0k9p9.apps.googleusercontent.com&scope=https://www.googleapis.com/auth/calendar&redirect_uri=http%3A//localhost:3000`;
   const loginAuth = () => {
     //window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=518141647017-6qje1u36g7vs8rq5bmivm4gk5uboksld.apps.googleusercontent.com&scope=openid%20email%20profile&redirect_uri=http%3A//localhost:3000/login`;
     window.location.href = urlScopeCalendar;
