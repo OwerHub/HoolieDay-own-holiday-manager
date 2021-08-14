1993254f8c2a6a03d3d05dbbcda20c1a20b0d401
OwerHub <owerwild@gmail.com>
2021-08-14 06:32:42 +0200

    HoldayDel Route OK

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ exports.testFunct = (req, res) => {
   res.send("im a testfunction in userController");
 };
 
+// new User
 exports.newUser = async (req, res) => {
   const newUser = new UserModel({
     name: req.body.name,
@@ -11,6 +12,63 @@ exports.newUser = async (req, res) => {
   });
 
   const response = await newUser.save();
+
+  res.send(response);
+};
+
+//  make a new Holyday
+exports.newHolyday = async (req, res) => {
+  const searchUser = await UserModel.findOne({
+    _id: req.body.id,
+  });
+
+  // valamiért hibás ID-nél kiakad
+  if (searchUser === null) {
+    res.send("User not find");
+  } else {
+    searchUser.holydays.push({
+      name: req.body.name,
+      date: req.body.date,
+      picture: req.body.picture,
+      celebrate: req.body.celebrate,
+      description: req.body.description,
+      type: req.body.type,
+    });
+
+    const response = await searchUser.save();
+
+    res.send(response);
+  }
+};
+
+// searchUser
+exports.searchUser = async (req, res) => {
+  const searchUser = await UserModel.findOne({
+    _id: req.query.data,
+  });
+
+  res.send(searchUser);
+};
+
+// deleteHolyday
+exports.deleteHolyday = async (req, res) => {
+  const deleteID = "6117426c58bc1f1afcf96da6";
+
+  const response = await UserModel.findOneAndUpdate(
+    {
+      "holydays._id": deleteID,
+    },
+    {
+      $pull: {
+        holydays: {
+          _id: deleteID,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  );
 
   res.send(response);
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,8 +6,16 @@ router.get("/ping", userController.testFunct);
 
 //new user
 router.post("/newUser", userController.newUser);
-//ask user by sub
+
+//ask user by sthg
+router.get("/searchuser", userController.searchUser);
 
 //ask all user
+
+// post new holyday
+router.post("/newholyday", userController.newHolyday);
+
+// delete holyday
+router.delete("/holyday", userController.deleteHolyday);
 
 module.exports = router;
