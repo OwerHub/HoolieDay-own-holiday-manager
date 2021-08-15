cf9827cf741c0c9adf591240a76c3f5b1dfefd5d
OwerHub <owerwild@gmail.com>
2021-08-15 06:05:25 +0200

    PersonTypes OK

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ const UserModel = require("../models/UserModel");
 exports.testFunct = (req, res) => {
   res.send("im a testfunction in userController");
 };
-
+// -----------USERS
 // new User
 exports.newUser = async (req, res) => {
   const newUser = new UserModel({
@@ -15,6 +15,46 @@ exports.newUser = async (req, res) => {
 
   res.send(response);
 };
+
+// searchUser
+exports.searchUser = async (req, res) => {
+  const searchUser = await UserModel.findOne({
+    _id: req.query.data,
+  });
+
+  res.send(response);
+};
+
+// UpdateUser
+exports.updateUser = async (req, res) => {
+  const response = await UserModel.findOneAndUpdate(
+    { _id: req.body.id },
+    {
+      $set: {
+        [req.body.key]: req.body.value,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.send(response);
+};
+
+// Delete User
+
+exports.deleteUser = async (req, res) => {
+  console.log(req.body.id);
+
+  const response = await UserModel.findOneAndDelete({
+    _id: req.body.id,
+  });
+
+  res.send(response);
+};
+
+//----------HOLYDAYS
 
 //  make a new Holyday
 exports.newHolyday = async (req, res) => {
@@ -41,27 +81,89 @@ exports.newHolyday = async (req, res) => {
   }
 };
 
-// searchUser
-exports.searchUser = async (req, res) => {
-  const searchUser = await UserModel.findOne({
-    _id: req.query.data,
-  });
+// UpdateHolyDay
+exports.updateHolyday = async (req, res) => {
+  const response = await UserModel.findOneAndUpdate(
+    {
+      "holydays._id": req.body.id,
+    },
+    { $set: { [`holydays.$.${req.body.key}`]: "its fuckin works de télleg" } },
+    { new: true }
+  );
 
-  res.send(searchUser);
+  res.send(response);
 };
 
 // deleteHolyday
 exports.deleteHolyday = async (req, res) => {
-  const deleteID = "6117426c58bc1f1afcf96da6";
+  const deleteID = "6117413aaccf6f18fcf29552";
 
   const response = await UserModel.findOneAndUpdate(
     {
-      "holydays._id": deleteID,
+      "holydays._id": req.query.id,
     },
     {
       $pull: {
         holydays: {
-          _id: deleteID,
+          _id: req.query.id,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.send(response);
+};
+
+//types
+
+//new Type
+exports.newPersonType = async (req, res) => {
+  const searchUser = await UserModel.findOne({
+    _id: req.body.id,
+  });
+
+  // valamiért hibás ID-nél kiakad de elvileg ilyet nem is kap
+  if (searchUser === null) {
+    res.send("User not find");
+  } else {
+    searchUser.types.push({
+      name: req.body.name,
+      color: req.body.color,
+      description: req.body.description,
+    });
+
+    const response = await searchUser.save();
+
+    res.send(response);
+  }
+};
+
+// update Type
+exports.updateType = async (req, res) => {
+  const response = await UserModel.findOneAndUpdate(
+    {
+      "types._id": req.body.id,
+    },
+    { $set: { [`types.$.${req.body.key}`]: req.body.value } },
+    { new: true }
+  );
+
+  res.send(response);
+};
+
+// delete Type
+exports.deleteType = async (req, res) => {
+  const response = await UserModel.findOneAndUpdate(
+    {
+      "types._id": req.body.id,
+    },
+    {
+      $pull: {
+        types: {
+          _id: req.body.id,
         },
       },
     },
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,18 +4,39 @@ const userController = require("../controllers/userController");
 
 router.get("/ping", userController.testFunct);
 
+//--- USERS
 //new user
 router.post("/newUser", userController.newUser);
 
 //ask user by sthg
 router.get("/searchuser", userController.searchUser);
 
-//ask all user
+//update User
+router.put("/updateUser", userController.updateUser);
+
+// delete user
+router.delete("/deleteUser", userController.deleteUser);
+
+//--- HOLYDAYS
 
 // post new holyday
 router.post("/newholyday", userController.newHolyday);
 
+// update holyday's stuf
+router.put("/updateHolyday", userController.updateHolyday);
+
 // delete holyday
 router.delete("/holyday", userController.deleteHolyday);
+
+//--- TYPES
+
+// new type
+router.get("/newType", userController.newPersonType);
+
+// update Type
+router.put("/updateType", userController.updateType);
+
+// delete Type
+router.delete("/deleteType", userController.deleteType);
 
 module.exports = router;
