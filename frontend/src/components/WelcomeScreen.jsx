17e4b4acce1d0f4d770e57d79e3e8b931a9e2f3d
OwerHub <owerwild@gmail.com>
2021-09-01 07:35:53 +0200

    Start git clean

--- frontend/src/components/WelcomeScreen.jsx
+++ frontend/src/components/WelcomeScreen.jsx
@@ -10,7 +10,24 @@ function WelcomeScreen(props) {
   return (
     <div className="WelcomeScreenOut">
       <div className="helloText">Hello User, Welcome in HoolieDays</div>
-      <div>Tudom, hogy rosszul írtam a Holiday-s mindenhol </div>
+      <div>
+        <p>
+          Lépj be és légy üdvözölve! <br></br> Ez egy réges-régi szerelemproject,
+          kissé átalakítva, hogy megfeleljen a Full Stack Api vizsga követelményeinek
+        </p>
+        <p>
+          A program front-end része a divatos "egységesen eklektikus " stílust
+          követi, amúgy számtalan kísérleti megoldásnak a gyakorlóterepe
+        </p>
+        <p>
+          Mivel a jelenlegi vizsga a backend részre koncentrál, a frontend-nek csak
+          kiszolgálószerep jut <br></br>
+          <em>
+            (a vizsga után, alighanem egy vagy több újonnan megtanult
+            keretrendszerrel fog új FE készülni)
+          </em>
+        </p>
+      </div>
       <div
         className="helloButton rainbowButtonOut height1rem"
         onClick={() => closeFunct()}
